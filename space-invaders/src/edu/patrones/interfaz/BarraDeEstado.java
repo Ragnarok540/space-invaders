0ab05af1f81e224a8da9161ad5f7fc0e56d139b0
Edgar Nova <ragnarok540@gmail.com>
2020-03-19 00:52:13 -0500

    Inicio Interfaz

---
+++ space-invaders/src/edu/patrones/interfaz/BarraDeEstado.java
@@ -0,0 +1,28 @@
+package edu.patrones.interfaz;
+
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+
+
+public class BarraDeEstado extends JPanel {
+	
+	private JLabel labelEstado;
+
+	public BarraDeEstado() {
+		super();
+		this.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.labelEstado = new JLabel("Cree o seleccione un jugador para empezar...");
+		this.labelEstado.setHorizontalAlignment(SwingConstants.LEFT);
+		this.add(labelEstado);
+	}
+
+	public void setEstado(String estado) {
+		this.labelEstado.setText(estado);
+	}
+	
+}
--- space-invaders/src/modelo/Enemigo.java
+++ space-invaders/src/modelo/Enemigo.java
@@ -1,3 +1,5 @@
+package modelo;
+
 public class Enemigo {
 
 }
